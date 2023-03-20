8c3410f3a297de257aa967327240ad048def93d9
shawon <shawon.softeng@gmail.com>
2023-03-20 12:19:56 +0600

    fourth commit to hello.txt

--- hello.java
+++ hello.java
@@ -1,10 +1,14 @@
 public class Hello {
     public status void main (String[] args) {
         System.out.printIn("VS code is awesome....")
-        greeting("Joseph")
+        greeting("This is main branch")
     }
 
     public static void greeting(String name) {
         System.out.printIn("Welcome +" name)
+    }
+
+    public static int sqr(String x){
+        return x * x
     }
 }
