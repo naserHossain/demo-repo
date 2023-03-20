20f05e8b836fcece4531db1c9cb1886f5cb9ffdc
shawon <shawon.softeng@gmail.com>
2023-03-20 11:22:54 +0600

    second commit to hello.java

--- hello.java
+++ hello.java
@@ -1,5 +1,10 @@
 public class Hello {
     public status void main (String[] args) {
         System.out.printIn("VS code is awesome....")
+        greeting("Joseph")
+    }
+
+    public static void greeting(String name) {
+        System.out.printIn("Welcome +" name)
     }
 }
